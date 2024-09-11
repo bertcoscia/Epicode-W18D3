16712a8095934afd647ea8274286631fb981363b
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-09-11 14:05:43 +0200

    connects to db, adds class Author

---
+++ src/main/java/bertcoscia/Epicode_W18D3/entities/Author.java
@@ -0,0 +1,26 @@
+package bertcoscia.Epicode_W18D3.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "authors")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Author {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+    private String name;
+    private String surname;
+    private String email;
+    @Column(name = "date_of_birth")
+    private LocalDate birthDate;
+    private String avatarUrl;
+}
